ce8abb326f57cc9cb37933569d31495d9fa9df3b
alokeinstein <110805623+alokeinstein@users.noreply.github.com>
2025-04-24 20:50:48 +0530

    Update CheckAuth.jsx

--- HairWig Project/Frontend/src/components/auth/CheckAuth.jsx
+++ HairWig Project/Frontend/src/components/auth/CheckAuth.jsx
@@ -3,11 +3,11 @@ import { useSelector } from "react-redux";
 
 function CheckAuth({ children }) {
   const location = useLocation();
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated, isLoading} = useSelector((state) => state.auth);
 
   // List of protected paths
   const protectedPaths = ['/cart', '/wishlist']; // Add more as needed
-
+if(isLoading) return "loading"
   if (!isAuthenticated && protectedPaths.some(path => location.pathname.includes(path))) {
     return <Navigate to="/login" state={{ from: location.pathname }} replace />;
   }
